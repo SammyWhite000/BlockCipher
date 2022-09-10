c89051a754e6eee2193eed85ae3afe494eef7173
Samuel White <Samuel.White@colostate.edu>
2022-09-09 19:13:18 -0600

    Updated ReadMe and run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-cd ../src
-make
-./Target
-make clean
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+start_message(){
+    printf "${GREEN}Starting Block Cipher Program${NC}\n"
+}
+
+execute_program(){
+    cd ../src
+    make
+    echo ""
+    printf "${GREEN}Beginning Tests!${NC}\n"
+    ./Target
+    make clean
+}
+
+end_message(){  
+    printf "${GREEN}Ending Block Cipher Program${NC}\n"
+}
+
+main(){
+    start_message
+    execute_program
+    echo ""
+    end_message
+}
+
+main
